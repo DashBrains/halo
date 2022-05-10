8069344a0739d81847eb607287b5fb225d29d8c2
frederic2ec <freddy.cho@hotmail.com>
2022-05-10 18:38:50 -0400

    fix: alert forward ref

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,32 +1,36 @@
-import * as React from 'react'
+import React from 'react'
 import { Alert as MUIAlert, AlertProps, styled } from '@mui/material'
 import Icon from '../Icon/Icon'
 import IconButton from '../IconButton/IconButton'
 
-const AlertBase = ({ iconMapping, onClose, action, ...rest }: AlertProps) => {
-  const customIconMapping = {
-    success: <Icon icon={'CheckCircle'} fontSize="inherit" />,
-    error: <Icon icon={'AlertCircle'} fontSize="inherit" />,
-    warning: <Icon icon={'AlertTriangle'} fontSize="inherit" />,
-    info: <Icon icon={'Info'} fontSize="inherit" />,
+const AlertBase = React.forwardRef<HTMLDivElement, AlertProps>(
+  ({ iconMapping, onClose, action, ...rest }, ref) => {
+    const customIconMapping = {
+      success: <Icon icon={'CheckCircle'} fontSize="inherit" />,
+      error: <Icon icon={'AlertCircle'} fontSize="inherit" />,
+      warning: <Icon icon={'AlertTriangle'} fontSize="inherit" />,
+      info: <Icon icon={'Info'} fontSize="inherit" />,
+    }
+
+    const onCloseButton = (
+      <IconButton size={'small'} color={'inherit'} onClick={onClose}>
+        <Icon icon={'X'} fontSize={'inherit'} color={'inherit'} />
+      </IconButton>
+    )
+
+    return (
+      <MUIAlert
+        iconMapping={iconMapping ? iconMapping : customIconMapping}
+        action={onClose ? onCloseButton : action}
+        ref={ref}
+        {...rest}
+      />
+    )
   }
+)
+AlertBase.displayName = 'AlertBase'
 
-  const onCloseButton = (
-    <IconButton size={'small'} color={'inherit'} onClick={onClose}>
-      <Icon icon={'X'} fontSize={'inherit'} color={'inherit'} />
-    </IconButton>
-  )
-
-  return (
-    <MUIAlert
-      iconMapping={iconMapping ? iconMapping : customIconMapping}
-      action={onClose ? onCloseButton : action}
-      {...rest}
-    />
-  )
-}
-
-const Alert = styled(AlertBase)((props) => {
+const AlertStyled = styled(AlertBase)((props) => {
   const shadowColor =
     props.theme.palette.mode && props.theme.palette.mode === 'dark'
       ? '255, 255, 255'
@@ -37,5 +41,10 @@ const Alert = styled(AlertBase)((props) => {
     boxShadow: `0 0px 6px 1px rgba(${shadowColor}, 0.1), 0 2px 4px 1px rgba(${shadowColor}, 0.06)`,
   }
 })
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>((props, ref) => (
+  <AlertStyled {...props} ref={ref} />
+))
+Alert.displayName = 'Alert'
 
 export default Alert
